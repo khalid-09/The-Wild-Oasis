d393cea2389ce99b695b3b73850afdd02cd68c05
Khalid Ansari <khaliljpc@gmail.com>
2023-12-01 23:22:35 +0530

    Added Supabase Client and API

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://waycrccmqafijtoknvap.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndheWNyY2NtcWFmaWp0b2tudmFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE0NDIxNjQsImV4cCI6MjAxNzAxODE2NH0.awTpuG6Oih2n24D3cUTFr5srlqXR69vBtXAzoMl7YfY';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
