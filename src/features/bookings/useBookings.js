0f3cb9689d6d9397965b7d40bc61cf85e3153f5f
Khalid Ansari <khaliljpc@gmail.com>
2023-12-09 22:57:50 +0530

    Added filter for bookings on API Side

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,24 @@
 import { useQuery } from '@tanstack/react-query';
 import { getBookings } from '../../services/apiBookings';
+import { useSearchParams } from 'react-router-dom';
 
 export const useBookings = () => {
+  const [searachParams] = useSearchParams();
+
+  // FILTER
+  const filterValue = searachParams.get('status');
+  const filter =
+    !filterValue || filterValue === 'all'
+      ? null
+      : { field: 'status', value: filterValue };
+
   const {
     data: bookings,
     isLoading,
     error,
   } = useQuery({
-    queryKey: ['bookings'],
-    queryFn: getBookings,
+    queryKey: ['bookings', filter],
+    queryFn: () => getBookings({ filter }),
   });
 
   return {
