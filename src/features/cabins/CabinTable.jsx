871009adb3760a85b3d839031feb4ca48df2fa50
Khalid Ansari <khaliljpc@gmail.com>
2023-12-09 16:09:11 +0530

    Added sorting for cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -14,12 +14,21 @@ const CabinTable = () => {
 
   const filterValue = searchParams.get('discount') || 'all';
 
+  // filtering cabins
   let filteredCabins;
   if (filterValue === 'all') filteredCabins = cabins;
   if (filterValue === 'no-discount')
     filteredCabins = cabins.filter(cabin => cabin.discount === 0);
   if (filterValue === 'with-discount')
     filteredCabins = cabins.filter(cabin => cabin.discount > 0);
+
+  // sorting cabins
+  const sortBy = searchParams.get('sortBy') || 'name-asc';
+  const [field, direction] = sortBy.split('-');
+  const modifier = direction === 'asc' ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
 
   return (
     <Menus>
@@ -33,7 +42,7 @@ const CabinTable = () => {
           <div></div>
         </Table.Header>
         <Table.Body
-          data={filteredCabins}
+          data={sortedCabins}
           render={cabin => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
