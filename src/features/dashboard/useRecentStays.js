d591cf979e2201636f40d33ae70efba29e7958ae
Khalid Ansari <khaliljpc@gmail.com>
2023-12-23 15:51:16 +0530

    Added Dashboard Statistics

--- src/features/dashboard/useRecentStays.js
+++ src/features/dashboard/useRecentStays.js
@@ -19,5 +19,5 @@ export const useRecentStays = () => {
     stay => stay.status === 'checked-in' || stay.status === 'checked-out'
   );
 
-  return { isLoading, stays, confirmedStays };
+  return { isLoading, stays, confirmedStays, numDays };
 };
